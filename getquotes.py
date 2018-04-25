8fa7fbbc07d0392a6f7ec51baaa978b6c5e8aae5
Ubuntu <devashree.mehta95@gmail.com>
2018-04-24 23:53:24 -0400

    initial

--- getquotes.py
+++ getquotes.py
@@ -9,15 +9,14 @@ import pandas as pd
 import time
 
 from itertools import izip, count
-import matplotlib.pyplot as plt
+
 # import plotly.plotly as py
 from numpy import linspace, loadtxt, ones, convolve
 import numpy as np
 import pandas as pd
 import collections
 from random import randint
-from matplotlib import style
-style.use('fivethirtyeight')
+
 
 
 intraday_cache={}
